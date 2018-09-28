6b97a734f005f79c3154b9eae8a9dfa3ade2110d
Nikunj Sathwara <nikunjsathwara039@gmail.com>
2018-09-28 08:12:19 -0700

    change

--- app.js
+++ app.js
@@ -1,82 +1,117 @@
-
-//Library Imports
-let express = require('express');
-let path = require('path');
-let cookieParser = require('cookie-parser');
-let bodyParser = require('body-parser');
-let Logger = require('./services/logger');
-let mongoose = require('mongoose');
-const cors = require('cors');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var publicIp = require('public-ip');
+var myPublicIp;
 var http = require('http');
 var fs = require('fs');
+var index = require('./routes/index');
 
-let app = express();
+var app = express();
 
-app.use(Logger.morgan);
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-//CORS
-app.use(cors({
-    origin: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Origin', ' X-Requested-With', ' Content-Type', ' Accept ', ' Authorization'],
-    credentials: true
-}));
+app.use(function (req, res, next) {
 
+  res.locals.myPublicIp = myPublicIp || '(Public IP has not been found)';
+
+  if (!myPublicIp) {
+    getPublicIp();
+  }
+
+  next();
+});
+
+app.use('/', index);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+function getPublicIp() {
+
+  publicIp.v4().then(ip => {
+    console.log(ip);
+    myPublicIp = ip;
+  });
+
+}
 var server = http.createServer(function (req, resp) {
-    //3.
-    if (req.url === "/firepad/userlist") {
-        fs.readFile("routes/examples/userlist.html", function (error, pgResp) {
-            if (error) {
-                resp.writeHead(404);
-                resp.write('Contents you are looking are Not Found');
-            } else {
-                resp.writeHead(200, { 'Content-Type': 'text/html' });
-                resp.write(pgResp);
-            }
-
-            resp.end();
-        });
-    } else if (req.url === "/firepad/richtext") {
-        fs.readFile("routes/examples/richtext.html", function (error, pgResp) {
-            if (error) {
-                resp.writeHead(404);
-                resp.write('Contents you are looking are Not Found');
-            } else {
-                resp.writeHead(200, { 'Content-Type': 'text/html' });
-                resp.write(pgResp);
-            }
-
-            resp.end();
-        });
-    } else if (req.url === "/firepad/richtextsimple") {
-        fs.readFile("routes/examples/richtext-simple.html", function (error, pgResp) {
-            if (error) {
-                resp.writeHead(404);
-                resp.write('Contents you are looking are Not Found');
-            } else {
-                resp.writeHead(200, { 'Content-Type': 'text/html' });
-                resp.write(pgResp);
-            }
-
-            resp.end();
-        });
-    } else {
-        //4.
+  //3.
+  if (req.url === "/firepad/userlist") {
+    fs.readFile("routes/examples/userlist.html", function (error, pgResp) {
+      if (error) {
+        resp.writeHead(404);
+        resp.write('Contents you are looking are Not Found');
+      } else {
         resp.writeHead(200, { 'Content-Type': 'text/html' });
-        resp.write('<h1>Product Manaager</h1><br /><br />To create product please enter: ' + req.url);
-        resp.end();
-    }
+        resp.write(pgResp);
+      }
+
+      resp.end();
+    });
+  } else if (req.url === "/firepad/richtext") {
+    fs.readFile("routes/examples/richtext.html", function (error, pgResp) {
+      if (error) {
+        resp.writeHead(404);
+        resp.write('Contents you are looking are Not Found');
+      } else {
+        resp.writeHead(200, { 'Content-Type': 'text/html' });
+        resp.write(pgResp);
+      }
+
+      resp.end();
+    });
+  } else if (req.url === "/firepad/richtextsimple") {
+    fs.readFile("routes/examples/richtext-simple.html", function (error, pgResp) {
+      if (error) {
+        resp.writeHead(404);
+        resp.write('Contents you are looking are Not Found');
+      } else {
+        resp.writeHead(200, { 'Content-Type': 'text/html' });
+        resp.write(pgResp);
+      }
+
+      resp.end();
+    });
+  } else {
+    //4.
+    resp.writeHead(200, { 'Content-Type': 'text/html' });
+    resp.write('<h1>Product Manaager</h1><br /><br />To create product please enter: ' + req.url);
+    resp.end();
+  }
 });
 //5.
 server.listen(5050);
 
-console.log('Server Started listening on 5050');
-//Routes Config
-let index = require('./routes/index');
-app.use('/', index);
-app.use(express.static(path.join(__dirname, 'upload')));
+getPublicIp();
+
 module.exports = app;
